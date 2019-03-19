9e0d78938bbdcc386187193656c887c86b0410fe
Saurabh Jagtap <saurabh619@outlook.com>
2019-03-19 17:57:44 +0530

    Create shellme.c

---
+++ shellme.c
@@ -0,0 +1,121 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#define BUFSIZE 1024
+#define TOK_BUFSIZE 64
+#define TOK_DELIM " "
+
+char *cmd_read(void){
+  	int bufsize = BUFSIZE;
+  	int position = 0;
+  	char *buffer = malloc(sizeof(char)*bufsize);
+  	int c;
+  	if (!buffer){
+    		fprintf(stderr, "allocation error\n");
+    		exit(EXIT_FAILURE);
+  	}
+  	while(1){// Read a character
+    		c = getchar();
+    		// If we hit EOF, replace it with a null character and return.
+    		if (c == EOF || c == '\n') {
+      			buffer[position] = '\0';
+      			return buffer;
+    		}
+		    else
+      		    buffer[position] = c;
+    		position++;
+    		// If we have exceeded the buffer, reallocate.
+    		if (position >= bufsize) {
+      			bufsize += BUFSIZE;
+      			buffer = realloc(buffer, bufsize);
+      			if (!buffer) {
+        			fprintf(stderr, "lsh: allocation error\n");
+        			exit(EXIT_FAILURE);
+      			}
+    		}
+  	}
+}
+
+
+char **cmd_args(char *line){
+  	int bufsize = TOK_BUFSIZE, position = 0;
+  	char **tokens = malloc(bufsize * sizeof(char*));
+  	char *token;
+  	if (!tokens) {
+    		fprintf(stderr, "allocation error\n");
+    		exit(EXIT_FAILURE);
+  	}
+  	token = strtok(line, TOK_DELIM);
+  	while (token != NULL) {
+    		tokens[position] = token;
+    		position++;
+		if (position >= bufsize) {
+      			bufsize += TOK_BUFSIZE;
+      			tokens = realloc(tokens, bufsize * sizeof(char*));
+      			if (!tokens) {
+        			fprintf(stderr, "allocation error\n");
+        			exit(EXIT_FAILURE);
+      			}
+    		}
+    		token = strtok(NULL, TOK_DELIM);
+  	}
+  	tokens[position] = NULL;
+  	return tokens;
+}
+
+
+int cmd_execute(char **args){
+    int i;
+  	if (args[0] == NULL){
+		printf("Write arguments");
+		return 1;	
+	}
+  	return cmd_go(args);
+}
+
+
+int cmd_exit(char **args){
+	return 0;
+}
+
+int cmd_go(char **args){
+  	pid_t pid, wpid;
+  	int status;
+  	pid = fork();
+  	if (pid == 0) {	// Child process
+    		if (execvp(args[0], args) == -1) 
+      			perror("HEllSHell>>");
+    		exit(EXIT_FAILURE);
+  	} 
+	else if (pid < 0) // Error forking
+    		perror("lsh");
+	else { // Parent process
+    		do {
+      			wpid = waitpid(pid, &status, WUNTRACED);
+    		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  	}
+  	return 1;
+}
+
+
+void cmd_loop(void){
+    char *line;
+  	char **args;
+  	int status;
+  	do{
+    		printf("> ");
+    		line = cmd_read();
+    		args = cmd_args(line);
+    		status = cmd_execute(args);
+    		free(line);
+    		free(args);
+	}while (status);
+}
+
+
+int main(int argc, char **argv){
+	cmd_loop();
+  	return EXIT_SUCCESS;
+}
